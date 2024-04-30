a2e7a5566422ab02c03bbca426cd23eda72308f9
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-04-30 10:24:10 +0900

    프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>앱임</h1>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import GlobalStyle from "./styles/GlobalStyle.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<>
+		<GlobalStyle />
+		<App />
+	</>,
+);
