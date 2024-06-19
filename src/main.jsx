eab7a00100116c08a92d44cd6a9daeaf3be57a26
hyunmin200 <gyejeongjin@gmail.com>
2024-06-19 12:27:59 +0900

    ADD :: 초기 세팅 완

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./font/font.css";
+import GlobalStyle from "./styles/GlobalStyle.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<>
+		<GlobalStyle />
+		<App />
+	</>,
+);
