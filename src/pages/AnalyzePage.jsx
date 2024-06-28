cecd83ad2467b3e4a3a122a3b496a8776f6d4834
hyunmin200 <gyejeongjin@gmail.com>
2024-06-28 12:29:42 +0900

    feat: NameBox

--- src/pages/AnalyzePage.jsx
+++ src/pages/AnalyzePage.jsx
@@ -5,7 +5,15 @@ function AnalyzePage() {
 	return (
 		<Main>
 			<SubTitle>AI 꿈 분석으로 당신의 꿈을 탐험해 보세요.</SubTitle>
-			<InputBox></InputBox>
+			<InputBG>
+				<InputBox>
+					<NameBox>
+						<NameTitle>사용자 이름 : </NameTitle>
+						<NameInput type="text" />
+					</NameBox>
+					<ContentBox></ContentBox>
+				</InputBox>
+			</InputBG>
 			<RButton>분석하기</RButton>
 		</Main>
 	);
@@ -26,7 +34,11 @@ const SubTitle = styled.h3`
 	font-size: 2rem;
 `;
 
-const InputBox = styled.form`
+const RButton = styled(Button)`
+	align-self: flex-end;
+`;
+
+const InputBG = styled.form`
 	width: 100%;
 	height: 520px;
 	border: 2px solid transparent;
@@ -36,6 +48,33 @@ const InputBox = styled.form`
 	background-clip: content-box, border-box;
 `;
 
-const RButton = styled(Button)`
-	align-self: flex-end;
+const InputBox = styled.div`
+	width: 100%;
+	height: 100%;
+	padding: 30px;
+	color: white;
 `;
+
+const NameBox = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const NameTitle = styled.h3`
+	width: 16rem;
+	min-width: 17rem;
+	font-size: 30px;
+	font-family: "PretendardB";
+`;
+
+const NameInput = styled.input`
+	width: 100rem;
+	min-width: 10rem;
+	padding: 5px;
+	border: 2px solid white;
+	color: white;
+	font-size: 24px;
+	font-family: "PretendardM";
+`;
+
+const ContentBox = styled.div``;
