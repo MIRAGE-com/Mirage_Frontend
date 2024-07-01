4221aa462290215b3aa74148d607c94b843589e3
hyunmin200 <gyejeongjin@gmail.com>
2024-07-01 12:19:15 +0900

    feat, chore: 꿈 작성 페이지 퍼블리싱 완 & axios 설치

--- src/pages/AnalyzePage.jsx
+++ src/pages/AnalyzePage.jsx
@@ -11,7 +11,10 @@ function AnalyzePage() {
 						<NameTitle>사용자 이름 : </NameTitle>
 						<NameInput type="text" />
 					</NameBox>
-					<ContentBox></ContentBox>
+					<ContentBox>
+						<ContentTitle>꿈 내용을 입력하세요!</ContentTitle>
+						<ContentInput />
+					</ContentBox>
 				</InputBox>
 			</InputBG>
 			<RButton>분석하기</RButton>
@@ -52,6 +55,9 @@ const InputBox = styled.div`
 	width: 100%;
 	height: 100%;
 	padding: 30px;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
 	color: white;
 `;
 
@@ -68,13 +74,44 @@ const NameTitle = styled.h3`
 `;
 
 const NameInput = styled.input`
-	width: 100rem;
+	width: 100%;
 	min-width: 10rem;
 	padding: 5px;
-	border: 2px solid white;
+	border-top: 2px solid white;
+	border-bottom: 2px solid white;
+	border-right: 0px;
+	border-left: 0px;
 	color: white;
 	font-size: 24px;
 	font-family: "PretendardM";
 `;
 
-const ContentBox = styled.div``;
+const ContentBox = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+`;
+
+const ContentTitle = styled.h3`
+	width: 100%;
+	min-width: 17rem;
+	font-size: 30px;
+	font-family: "PretendardB";
+`;
+
+const ContentInput = styled.textarea`
+	width: 100%;
+	height: 100%;
+	min-width: 10rem;
+	padding: 5px;
+	border-top: 2px solid white;
+	border-bottom: 2px solid white;
+	border-right: 0px;
+	border-left: 0px;
+	outline: none;
+	background-color: transparent;
+	color: white;
+	font-size: 24px;
+	font-family: "PretendardM";
+`;
